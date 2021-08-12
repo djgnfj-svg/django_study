9d358645c69e812fe0110475d8ca220ec3b195dd
djgnfj-svg <djgnfj8923@naver.com>
2021-08-13 02:03:00 +0900

    mistake:

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,4 @@
-from accountapp.views import AccountCreateView, hello_world
+from accountapp.views import AccountCreateView, AccountDeleteView, AccountDetailView, AccountUpdateView, hello_world
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -9,6 +9,9 @@ urlpatterns = [
 
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('create/', AccountCreateView.as_view(), name='create'),
 
+    path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,15 +1,26 @@
+from accountapp.decorators import account_ownership_required
+from django.views.generic.edit import DeleteView
+from accountapp.forms import AccountUpdateForm
+from accountapp.models import HelloWorld
 from django.http.response import HttpResponseRedirect
 from django.urls.base import reverse, reverse_lazy
-from accountapp.models import HelloWorld
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 
+has_ownership = [
+    account_ownership_required, login_required
+]
+
+
+@login_required
 def hello_world(request):
 
     if request.method == "POST":
@@ -30,3 +41,28 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(CreateView):
+    model = User
+    context_object_name = 'target_user'
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
