203c1821b373680cd033cb1f835df385f595a808
djgnfj-svg <djgnfj8923@naver.com>
2021-08-16 00:22:10 +0900

    33

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.urls.base import reverse
 from django.views.generic.edit import CreateView, UpdateView
 from django.utils.decorators import method_decorator
 
@@ -13,7 +14,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -22,6 +22,9 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -29,5 +32,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
