0217650d46c7f2b31379cce9cdaf26e32a7c2a9f
djgnfj-svg <djgnfj8923@naver.com>
2021-08-18 00:01:57 +0900

    35

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,7 +1,21 @@
 
 
+from articleapp.views import ArticleCreateView, ArticleDeleteView, ArticleDetatilView, ArticleUpdateView
 from django.urls.conf import path
 from django.views.generic import TemplateView
+
+app_name = 'articleapp'
 urlpatterns = [
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list')
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+
+    path('create/', ArticleCreateView.as_view(template_name='articleapp/create.html'), name='create'),
+    path('detail/<int:pk>', ArticleDetatilView.as_view(
+        template_name='articleapp/detail.html'), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(
+        template_name='articleapp/update.html'), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(
+        template_name='articleapp/delete.html'), name='delete'),
+
+
+
 ]
