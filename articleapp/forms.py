0761c17edfdfc79809f0815dabf1c533af0e92b3
djgnfj-svg <djgnfj8923@naver.com>
2021-08-26 11:45:42 +0900

    bootstrap fix

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -8,7 +8,8 @@ from projectapp.models import Project
 class ArticleCreationForm(ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left',
                                                            'style': 'height: auto;'}))
-    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
+    project = forms.ModelChoiceField(
+        queryset=Project.objects.all(), required=False)
 
     class Meta:
         model = Article
